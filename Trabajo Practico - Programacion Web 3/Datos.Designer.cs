0a0d44ecd57a100ce18964c382a8fdc9ba8f6d79
Daniela <danieladvm@yahoo.com.ar>
2015-06-15 23:38:06 -0300

    Agregado el EDMX
    Ya podemos empezar a trabajar con la base

---
+++ Trabajo Practico - Programacion Web 3/Datos.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\Danielita\Documents\Visual Studio 2012\Projects\TP-PW3\Trabajo Practico - Programacion Web 3\Datos.edmx'. 
+// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
+// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
+// el modelo en el diseñador.
