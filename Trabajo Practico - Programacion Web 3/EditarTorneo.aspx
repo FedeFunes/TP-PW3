cdbf5826b9b28640a3e27049b1a42c9c16e024cf
fefofunes <federicofunes92@gmail.com>
2015-06-20 17:48:29 -0300

    nueva pagina editarTorneo

---
+++ Trabajo Practico - Programacion Web 3/EditarTorneo.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Administracion.master" AutoEventWireup="true" CodeBehind="EditarTorneo.aspx.cs" Inherits="Trabajo_Practico___Programacion_Web_3.EditarTorneo" %>
+<asp:Content ID="contenidoHead" ContentPlaceHolderID="head" runat="server">
+    <script src="js/Atorneos.js"></script>
+</asp:Content>
+<asp:Content ID="Content1" ContentPlaceHolderID="PageContainer" runat="server">
+<!-- Page Content -->
+<div class="container">
+    <div class="contenido">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="form-group">
+                    <label for="txtNombreTorneo">Nombre</label>
+                    <asp:TextBox ID="txtNombreTorneo" runat="server" CssClass="form-control" placeholder="Ingrese nombre del torneo"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="ValidaNombreTorneo1" runat="server" ErrorMessage="Campo Nombre: No puede estar vacío:" Text="(*)" ControlToValidate="txtNombreTorneo" Display="Dynamic" EnableClientScript="false"></asp:RequiredFieldValidator>
+                    <asp:RegularExpressionValidator ID="ValidaNombreTorneo2" runat="server" ErrorMessage="Campo Nombre: Solo letras." Text="(*)" ValidationExpression="([A-Z]|[a-z]|\s)+" ControlToValidate="txtNombreTorneo" Display="Dynamic" EnableClientScript="false"></asp:RegularExpressionValidator>
+                    <span class="errorForm text-danger" id="errorNombreTorneo">Error: Campo vacio o inválido (deben ser solo letras).</span> 
+                </div>
+               
+                <div class="checkbox">
+                    <asp:CheckBox ID="cbActivoTorneo" runat="server" Checked="true" Text="Activo" />
+                </div>
+
+                <asp:ValidationSummary ID="ValidationSummary1" runat="server" HeaderText="Errores en formulario:" />
+                
+                <asp:Button ID="btnEditarTorneo" runat="server" Text="Editar Torneo" CssClass="btn btn-default"/> 
+            </div>
+        </div>
+        <!-- /.row -->
+    </div>
+</div>
+<!-- /.container -->
+</asp:Content>
+
---
+++ Trabajo Practico - Programacion Web 3/EditarTorneo.aspx.designer.cs
@@ -0,0 +1,69 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Trabajo_Practico___Programacion_Web_3 {
+    
+    
+    public partial class EditarTorneo {
+        
+        /// <summary>
+        /// Control txtNombreTorneo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreTorneo;
+        
+        /// <summary>
+        /// Control ValidaNombreTorneo1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator ValidaNombreTorneo1;
+        
+        /// <summary>
+        /// Control ValidaNombreTorneo2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator ValidaNombreTorneo2;
+        
+        /// <summary>
+        /// Control cbActivoTorneo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox cbActivoTorneo;
+        
+        /// <summary>
+        /// Control ValidationSummary1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary ValidationSummary1;
+        
+        /// <summary>
+        /// Control btnEditarTorneo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEditarTorneo;
+    }
+}
