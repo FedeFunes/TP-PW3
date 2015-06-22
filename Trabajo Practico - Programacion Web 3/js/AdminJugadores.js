4f80d523f3bc565ae32a8c48b9492a8884fc043d
fefofunes <federicofunes92@gmail.com>
2015-06-22 20:19:54 -0300

    js's para las validaciones de los admin

---
+++ Trabajo Practico - Programacion Web 3/js/AdminJugadores.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    $("#PageContainer_PageContainer_btnEditar").click(function () {
+
+        var txtIdJugador = $("#PageContainer_PageContainer_txtIdJugador").val();
+        var errorIdJugador = $("#errorIdJugador");
+
+
+        if (txtIdJugador == null || txtIdJugador.length == 0 || !/^\d+$/.test(txtIdJugador)) {
+            errorIdJugador.css("display", "inline");
+            return false;
+        } else {
+            errorIdJugador.css("display", "none");
+        }
+    });
+
+    $("#btnEliminarModal").click(function () {
+
+        var txtIdJugador = $("#PageContainer_PageContainer_txtIdJugador").val();
+        var errorIdJugador = $("#errorIdJugador");
+
+
+        if (txtIdJugador== null || txtIdJugador.length == 0 || !/^\d+$/.test(txtIdJugador)) {
+            errorIdJugador.css("display", "inline");
+            return false;
+        } else {
+            errorIdJugador.css("display", "none");
+        }
+    });
+});
